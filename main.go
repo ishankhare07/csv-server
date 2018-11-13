514fe41ec8e8d1d33bb6cfa664db4f77d3aecc23
Ishan Khare <ishankhare07@gmail.com>
2018-11-13 13:02:20 +0530

    read file once into a map
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/csv"
-	"errors"
 	"fmt"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/middleware/logger"
@@ -12,7 +11,9 @@ import (
 	"strings"
 )
 
-func getCSVData(key string) (string, error) {
+func loadCSVData() map[string]string {
+	csvData := make(map[string]string)
+
 	file, err := os.Open("Corpus.csv")
 
 	if err != nil {
@@ -31,15 +32,15 @@ func getCSVData(key string) (string, error) {
 			panic(fmt.Sprintf("err in reading csv: %s", err))
 		}
 
-		if strings.TrimSpace(record[0]) == key {
-			return strings.TrimSpace(record[1]), nil
-		}
+		csvData[strings.TrimSpace(record[0])] = strings.TrimSpace(record[1])
 	}
 
-	return "", errors.New("Not Found!")
+	return csvData
 }
 
 func main() {
+	csvData := loadCSVData()
+
 	app := iris.New()
 	app.Logger().SetLevel("debug")
 
@@ -47,20 +48,20 @@ func main() {
 	app.Use(logger.New())
 
 	app.Handle("GET", "/{key}", func(ctx iris.Context) {
-		value, err := getCSVData(ctx.Params().Get("key"))
+		value, ok := csvData[ctx.Params().Get("key")]
 
-		if err != nil {
-			ctx.StatusCode(iris.StatusNotFound)
-			ctx.JSON(iris.Map{
-				"error": err.Error(),
-			})
-		} else {
+		if ok {
 			ctx.JSON(iris.Map{
 				"key":   ctx.Params().Get("key"),
 				"value": value,
 			})
+		} else {
+			ctx.StatusCode(iris.StatusNotFound)
+			ctx.JSON(iris.Map{
+				"error": "Not Found!",
+			})
 		}
 	})
 
-	app.Run(iris.Addr("0.0.0.0:8080"), iris.WithoutServerError(iris.ErrServerClosed))
+	app.Run(iris.Addr("0.0.0.0:80"), iris.WithoutServerError(iris.ErrServerClosed))
 }
